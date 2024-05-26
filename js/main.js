4309295ab68b7aca73ca2df21dc278afe22a03d8
Sulvrix_ <sultanbamarhool@gmail.com>
2024-05-26 07:08:26 +0300

    Finish

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function ToggleNavBar() {
-  var checkBox = $("#checkbox1");
+$(document).ready(function () {
   var navbar = $("#navbarNav");
-
-  if (checkBox.is(":checked") == true) {
+  if ($(window).width() >= 1000) {
+    navbar.removeAttr("style");
+  } else {
     navbar.css({ height: "100vh" });
     navbar.css({ width: "100vw" });
     navbar.css({ margin: "0" });
@@ -14,11 +14,21 @@ function ToggleNavBar() {
     navbar.css({ alignItems: "center" });
     navbar.css({ gap: "50px" });
     navbar.css({ right: "0" });
-    navbar.css({ top: "100%" });
+    navbar.css({ top: "-1100%" });
     navbar.css({ position: "absolute" });
     navbar.css({ backgroundColor: "rgb(240, 240, 240)" });
+    navbar.css({ transition: "1s ease-in-out" });
+  }
+});
+
+function ToggleNavBar() {
+  var checkBox = $("#checkbox1");
+  var navbar = $("#navbarNav");
+
+  if (checkBox.is(":checked") == true) {
+    navbar.css({ top: "100%" });
   } else {
-    navbar.css({ display: "none" });
+    navbar.css({ top: "-1100%" });
   }
 }
 
@@ -28,5 +38,21 @@ $(window).on("resize", function () {
 
   if (win.width() >= 1000) {
     navbar.removeAttr("style");
+  } else {
+    navbar.css({ height: "100vh" });
+    navbar.css({ width: "100vw" });
+    navbar.css({ margin: "0" });
+    navbar.css({ padding: "0" });
+    navbar.css({ display: "flex" });
+    navbar.css({ flexDirection: "column" });
+    navbar.css({ justifyContent: "center" });
+    navbar.css({ alignContent: "center" });
+    navbar.css({ alignItems: "center" });
+    navbar.css({ gap: "50px" });
+    navbar.css({ right: "0" });
+    navbar.css({ top: "-1100%" });
+    navbar.css({ position: "absolute" });
+    navbar.css({ backgroundColor: "rgb(240, 240, 240)" });
+    navbar.css({ transition: "1s ease-in-out" });
   }
 });
